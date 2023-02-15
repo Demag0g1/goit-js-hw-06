b3232c25f2facdf582001255d189c85f01f4cbf1
Demag0g1 <konievanatol@gmail.com>
2023-02-15 22:43:06 +0200

    Revert "for review after processing"
    This reverts commit 401037a3bc8f2a2010138dbdb279d75ee87b29ca.

---
+++ goit-js-hw-06/js/task-07.js
@@ -0,0 +1,9 @@
+// Напиши скрипт, який реагує на зміну значення input#font - size - control(подія input) і змінює інлайн - стиль span#text,
+// оновлюючи властивість font - size. В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
+
+const sizeControl = document.querySelector("input#font-size-control");
+const textChange = document.querySelector("span#text");
+
+sizeControl.addEventListener("input", (event) => {
+  textChange.style.fontSize = `${event.target.value}px`;
+});
