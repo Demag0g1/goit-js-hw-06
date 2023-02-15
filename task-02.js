800d405a9b3c3ff990635bc300b514c3e54a1da2
Demag0g1 <112280688+Demag0g1@users.noreply.github.com>
2023-02-15 21:05:50 +0200

    update

---
+++ task-02.js
@@ -0,0 +1,24 @@
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+//     Створить окремий елемент < li>. Обов'язково використовуй метод document.createElement().
+//     Додасть назву інгредієнта як його текстовий вміст.
+//     Додасть елементу клас item.
+//     Після чого, вставить усі < li > за одну операцію у список ul#ingredients.
+
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+const listRef = document.querySelector("#ingredients");
+const listItemArr = [];
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  listItemArr.push(listItem);
+});
+listRef.append(...listItemArr);
---
+++ task-05.js
@@ -0,0 +1,14 @@
+// Напиши скрипт, який під час набору тексту в інпуті input#name - input(подія input), підставляє його поточне значення в span#name - output.
+// Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
+
+const refs = {
+  input: document.querySelector("#name-input"),
+  output: document.querySelector("#name-output"),
+};
+document.querySelector("#name-input").value = "";
+
+refs.input.addEventListener("input", (event) => {
+  refs.output.textContent = event.currentTarget.value;
+
+  if (event.target.value === "") refs.output.textContent = "Anonymous";
+});
